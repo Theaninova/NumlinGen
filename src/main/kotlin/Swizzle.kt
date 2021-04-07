c373a62811264b78b0342390da8a56de4c5aa1c4
Wieland Schöbl <wulkanat@gmail.com>
2021-04-07 19:13:06 +0200

    Add swizzle

---
+++ src/main/kotlin/Swizzle.kt
@@ -0,0 +1,45 @@
+import java.io.File
+
+fun permutations(chars: String) =
+    chars.flatMap { c1 ->
+        if (c1 != ' ') chars.flatMap { c2 ->
+            if (c2 != ' ') chars.flatMap { c3 ->
+                if (c3 != ' ') chars.map { c4 ->
+                    if (c4 != ' ') "$c1$c2$c3$c4" else "$c1$c2$c3"
+                } else listOf("$c1$c2")
+            } else listOf("$c1")
+        } else listOf()
+    }
+
+val xyzwIndex = mapOf(
+    'x' to 0,
+    'y' to 1,
+    'z' to 2,
+    'w' to 3,
+)
+val xyzwPermutations = permutations("xyzw ")
+val rgbwIndex = mapOf(
+    'r' to 0,
+    'g' to 1,
+    'b' to 2,
+    'w' to 3,
+)
+val rgbwPermutations = permutations("rgb  ")
+
+fun main() {
+    val file = File("gen", "Swizzle.kt")
+    // @formatter:off
+    val content =
+"""@file:Suppress("unused", "MemberVisibilityCanBePrivate")
+
+package vector
+
+/// GLSL-style swizzle operations
+${types.joinToString("\n\n") { type -> xyzwPermutations.joinToString("\n") { permutation ->
+"val ${type}Vector.$permutation get() = ${type}Vector(${type.toLowerCase()}ArrayOf(${permutation.map { xyzwIndex[it] }
+    .joinToString(", ") { "this[$it]"}}))"
+}}}
+"""
+    // @formatter:on
+    file.writeText(content)
+}
